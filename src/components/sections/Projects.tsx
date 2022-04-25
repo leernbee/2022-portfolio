68161783db793fc87aa2b4f1b5241611c123f827
leernbee <leernbee@gmail.com>
2022-04-25 18:19:00 +0800

    optimize images

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -19,38 +19,38 @@ const Projects = () => {
             link="https://paymongo-landing-page-clone.vercel.app/"
             title="Paymongo Clone"
             desc="A clone of landing page made with Tailwind from Webflow"
-            image="/assets/images/paymongo.png"
+            image="/assets/images/paymongo.jpg"
           />
 
           <ProjectCard
             link="https://firstx.ai"
             title="FirstX Conversation using Twitter API"
             desc="A streamlined Twitter outreach marketing technology platform"
-            image="/assets/images/firstx.png"
+            image="/assets/images/firstx.jpg"
           />
           <ProjectCard
             link="https://vinerahub.com"
             title="Vinerahub Wine Experience Booking Platform"
             desc="A wine experience booking platform built with Laravel"
-            image="/assets/images/vinerahub.png"
+            image="/assets/images/vinerahub.jpg"
           />
           <ProjectCard
             link="https://icomply.ph"
             title="iComply Business Case Study"
             desc="iComply is an End-to-end services from incorporation to accounting"
-            image="/assets/images/icomply.png"
+            image="/assets/images/icomply.jpg"
           />
           <ProjectCard
             link="https://growtechfarms.com/"
             title="Growtech Farms Shopify eCommerce"
             desc="Growtech Farms was established to elevate the Agriculture sector"
-            image="/assets/images/growtech.png"
+            image="/assets/images/growtech.jpg"
           />
           <ProjectCard
             link="https://magkaisa.ph"
             title="Election Cheatsheet with Social Share"
             desc="A personal project in connection with the upcoming election"
-            image="/assets/images/magkaisa.png"
+            image="/assets/images/magkaisa.jpg"
           />
         </div>
       </div>
